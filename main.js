18b63d749a56bb7616f7e907ef36855bd485034c
Leonardo Santos Azevedo <leonardosantosazevedo195@gmail.com>
2024-02-23 16:52:55 -0300

    first commit

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const nameElement = document.querySelector('#name');
     const usernameElement = document.querySelector('#username');
     const avatarElement = document.querySelector('#avatar');
-    const reposElement = document.querySelector('#repos');
     const followersElement = document.querySelector('#followers');
     const followingElement = document.querySelector('#following');
     const linkElement = document.querySelector('#link');
